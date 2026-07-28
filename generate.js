9067741c6879cecfd2a8bff44b9fa04c1fb14d47
foodiemls <junting.topscholar@gmail.com>
2026-07-28 13:32:45 +0800

    Update generate.js

--- generate.js
+++ generate.js
@@ -281,3 +281,80 @@ item.id + ".html"
 
 
 });
+
+// ==========================
+// Generate sitemap.xml
+// ==========================
+
+
+let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+<url>
+
+<loc>
+https://foodiemls.github.io/KL-FOOD-GUIDE/
+</loc>
+
+<changefreq>
+weekly
+</changefreq>
+
+<priority>
+1.0
+</priority>
+
+</url>
+`;
+
+
+
+restaurants.forEach(function(item){
+
+
+sitemap += `
+
+<url>
+
+<loc>
+https://foodiemls.github.io/KL-FOOD-GUIDE/restaurants/${item.id}.html
+</loc>
+
+<changefreq>
+monthly
+</changefreq>
+
+<priority>
+0.8
+</priority>
+
+</url>
+
+`;
+
+
+
+});
+
+
+sitemap += `
+
+</urlset>
+
+`;
+
+
+
+fs.writeFileSync(
+
+"sitemap.xml",
+
+sitemap
+
+);
+
+
+console.log(
+"Sitemap generated"
+);
